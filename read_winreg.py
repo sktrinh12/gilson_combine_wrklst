67f7052250d82f6edfe6268e9e55979d1f0cb1e7
Spencer Trinh <trinh.kcs85@gmail.com>
2020-07-24 00:10:55 -0400

    type in calling a function without the namespace

--- read_winreg.py
+++ read_winreg.py
@@ -2,6 +2,7 @@ import winreg
 import sys
 
 
+WORKLIST_FILEPATH = 'C:\\Users\\Tecan\\Documents\\gilson_app_files\\WORKLIST_FILEPATH.txt'
 reg_path = r"Volatile Environment"
 reg_key_val = r"WORKLIST_FILEPATH"
 
@@ -21,8 +22,16 @@ def query_registry(reg_path, reg_key_val):
 def check_reg_path_exists(reg_path):
     access_registry = winreg.ConnectRegistry(None,winreg.HKEY_CURRENT_USER) # If None, the local computer is used
     try:
-        access_key = OpenKey(access_registry, reg_path)
+        access_key = winreg.OpenKey(access_registry, reg_path)
         winreg.CloseKey(access_key)
         return True
     except EnvironmentError:
         return False
+
+
+if __name__ == "__main__":
+    if check_reg_path_exists(reg_path):
+        filepath = query_registry(reg_path, reg_key_val)
+        with open(WORKLIST_FILEPATH, 'w', encoding='utf-8') as f:
+            f.write(filepath)
+        print('new file path updated!')
