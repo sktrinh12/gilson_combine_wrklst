2372b50fbc6dfe9c9e8b48f17eaa09e3f9d99116
Spencer Trinh <trinh.kcs85@gmail.com>
2020-08-08 14:03:47 -0400

    simplified conext mgmt statement

--- app/worker.py
+++ app/worker.py
@@ -4,11 +4,12 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL',
+                      'redis://localhost:6379').strip().replace('"', '')
 
-conn = redis.from_url(redis_url)
+print(f'redis url: {redis_url}')
 
 if __name__ == '__main__':
-    with Connection(conn):
+    with Connection(redis.from_url(redis_url)):
         worker = Worker(list(map(Queue, listen)))
         worker.work()
